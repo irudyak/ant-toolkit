b2669ba103bac466076fe5cbc0060c34c62de333
Igor <irudyak@gmail.com>
2015-08-11 21:46:56 -0700

    aws.ec2.deleteVpc task added

--- dev/modules/native/aws-ec2/src/main/java/com/anttoolkit/aws/ec2/tasks/vpc/DeleteVpcTask.java
+++ dev/modules/native/aws-ec2/src/main/java/com/anttoolkit/aws/ec2/tasks/vpc/DeleteVpcTask.java
@@ -1,5 +1,32 @@
 package com.anttoolkit.aws.ec2.tasks.vpc;
 
-public class DeleteVpcTask
+import com.amazonaws.services.ec2.model.*;
+
+import org.apache.tools.ant.*;
+
+import com.anttoolkit.aws.ec2.tasks.*;
+
+public class DeleteVpcTask extends GenericEc2Task
 {
+	private DeleteVpcRequest request = new DeleteVpcRequest();
+
+	public void setVpcId(String id)
+	{
+		request.setVpcId(id);
+	}
+
+	@Override
+	public void doWork() throws BuildException
+	{
+		getEc2Client().deleteVpc(request);
+	}
+
+	@Override
+	protected void validate()
+	{
+		if (request.getVpcId() == null || request.getVpcId().trim().isEmpty())
+		{
+			throw new BuildException("VPC id should be specified");
+		}
+	}
 }
