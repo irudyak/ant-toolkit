e02b8274a78e350dfb2790fff19ba8f96540bc49
Igor <irudyak@gmail.com>
2015-08-26 16:16:09 -0700

    aws.ec2.deleteRouteTable task added

---
+++ dev/modules/native/aws-ec2/src/main/java/com/anttoolkit/aws/ec2/tasks/routetable/DeleteRouteTableTask.java
@@ -0,0 +1,23 @@
+package com.anttoolkit.aws.ec2.tasks.routetable;
+
+import com.amazonaws.services.ec2.model.*;
+
+import org.apache.tools.ant.*;
+
+import com.anttoolkit.general.common.annotations.*;
+import com.anttoolkit.aws.ec2.tasks.*;
+
+public class DeleteRouteTableTask extends GenericEc2Task
+{
+	@Required("Route table id should be specified")
+	private String tableId;
+
+	@Override
+	public void doWork() throws BuildException
+	{
+		DeleteRouteTableRequest request = new DeleteRouteTableRequest();
+		request.setRouteTableId(tableId);
+
+		getEc2Client().deleteRouteTable(request);
+	}
+}
